761a298172c05e21d84a55fe67907a728835defa
Vivek Singh <hiviveksingh@gmail.com>
2023-12-19 19:26:02 -0500

    added new file from leetcode

---
+++ src/Leethcode/seventyfive/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package Leethcode.seventyfive.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+	
+		System.out.println(solveIt("pwwkew"));
+
+	}
+
+	private static int solveIt(String s) {
+	    int i = 0, j = 0, max = 0;
+	    Set<Character> set = new HashSet<>();
+	    
+	    while (j < s.length()) {
+	        if (!set.contains(s.charAt(j))) {
+	            set.add(s.charAt(j++));
+	            max = Math.max(max, set.size());
+	        } else {
+	            set.remove(s.charAt(i++));
+	        }
+	    }
+	    
+	    return max;
+	}
+}
