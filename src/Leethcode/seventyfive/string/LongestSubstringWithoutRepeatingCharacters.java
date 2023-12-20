b4b011ff41b79aa4f72ffb24354089968ec5b57b
Vivek Singh <hiviveksingh@gmail.com>
2023-12-19 20:14:28 -0500

    fixed

--- src/Leethcode/seventyfive/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/Leethcode/seventyfive/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,17 +1,47 @@
 package Leethcode.seventyfive.string;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public static void main(String[] args) {
-	
+
 		System.out.println(solveIt("pwwkew"));
 
 	}
 
-	private static int solveIt(String s) {
+	private static Map<Integer,Set<Character>>   solveIt(String s) {
+	   
+	    Map<Integer,Set<Character>> map = new HashMap<Integer,Set<Character>>();
+	    
+	    char[] charArray = s.toCharArray();
+	   
+	    for (int i = 0; i < charArray.length; i++) {
+			
+			
+			Set<Character> set = new LinkedHashSet <>();
+			map.put(i, set);
+			
+			for (int k = i; k < charArray.length; k++) {
+				char ch = charArray[k];
+				if (!set.contains(ch)) {
+					set.add(ch);
+				}else {
+					break;
+				}
+			 }
+			
+		}
+	  
+	    return map;
+	}
+
+	//  leetversion
+	public int lengthOfLongestSubstring(String s) {
 	    int i = 0, j = 0, max = 0;
 	    Set<Character> set = new HashSet<>();
 	    
