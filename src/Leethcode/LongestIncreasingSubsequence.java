c6a5891ab7a47efa22acc589a326fa8e4224d2ee
Vivek Singh <hiviveksingh@gmail.com>
2024-01-14 10:35:33 -0500

    Coin prob

--- src/Leethcode/LongestIncreasingSubsequence.java
+++ src/Leethcode/LongestIncreasingSubsequence.java
@@ -13,19 +13,37 @@ public class LongestIncreasingSubsequence {
 
 	//	System.out.println(solve(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 }));
 		
-		System.out.println(solve(new int[] { 10, 9, 3, 5, 2, 7, 101, 18 }));  //-[2, 5, 7, 101]
-
+		System.out.println("solved value :: "+solve(new int[] { 10, 9, 3, 5, 2, 7, 101, 18 }));  //-[2, 5, 7, 101]
+		System.out.println("solved value :: "+lengthOfLISDP(new int[] { 10, 9, 3, 5, 2, 7, 101, 18 }));  //-[2, 5, 7, 101]//Dynamic programming
+ 
 		//System.out.println(solve(new int[] { 0, 1, 0, 3, 2, 3 }));
 
 		//System.out.println(solve(new int[] { 7, 7, 7, 7, 7, 7 }));
 
 	}
-
 	private static int solve(int[] intArray) {
 
 		return lengthOfLISBest(intArray);
 	}
 
+	public static  int  lengthOfLISDP(int[] nums) {
+        
+        int [] Lis = new int[nums.length];
+        Arrays.fill(Lis, 1);
+        int max=1;
+        
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    Lis[i] = Math.max(Lis[i], 1+Lis[j]);
+                    max = Math.max(max, Lis[i]);
+                }
+            }
+        }
+        
+        return max;
+        
+    }
 	static int lengthOfLISBest(int[] intArry) {
 		
 		Integer[] LHSCountByIndex = new Integer [intArry.length];	
@@ -36,16 +54,16 @@ public class LongestIncreasingSubsequence {
 
 		for (int i = intArry.length - 1; i >= 0; i--) {
 			
-			System.out.println("  For I " + intArry[i]);
-			System.out.println("  Value of J ");
+		//	System.out.println("  For I " + intArry[i]);
+		//	System.out.println("  Value of J ");
 			
 			for (int j = i + 1; j < intArry.length; j++) {
 			
-				System.out.print(" " + intArry[j]);
+			//	System.out.print(" " + intArry[j]);
 				
 				if(intArry[i] < intArry[j]) {
 					
-					System.out.println( "   "+LHSCountByIndex[i]+" i index value j " + LHSCountByIndex[j]);
+				//	System.out.println( "   "+LHSCountByIndex[i]+" i index value j " + LHSCountByIndex[j]);
 					
 					LHSCountByIndex[i] =  Math.max( LHSCountByIndex[i], 1 + LHSCountByIndex[i])	;
 					
@@ -99,5 +117,8 @@ public class LongestIncreasingSubsequence {
 		System.out.println(lo +"-bs-"+list);
 		return lo;
 	}
+	
+	
+
 
 }
